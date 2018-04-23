4b4bf3f60877c1834492bca19a077a2314007cd6
Oleksandr1296 <olkravchukv@gmail.com>
2018-04-23 14:41:04 +0300

    add instantaneous language change

--- localization/localization.js
+++ localization/localization.js
@@ -7,28 +7,41 @@ var getCurrentLanguage = function () {
     defaultLanguage = localStorage.getItem('language');
   }
   return defaultLanguage;
-}
-var currentLang = getCurrentLanguage().toLowerCase();
+};
+let currentLang = getCurrentLanguage().toLowerCase();
 var langEls = document.getElementsByClassName('lang-' + currentLang);
 
 for (var i=0; i<langEls.length; i++) {
-  var langEl = langEls[i];
+  let langEl = langEls[i];
   langEl.classList.add('visible');
 }
 document.getElementById(currentLang).checked = true;
 // $<prefix> == DOMElement
 var $save = document.querySelector('html body button#save')
 $save.addEventListener('click', function() {
-  var lastLangEls = document.getElementsByClassName('lang-' + localStorage.getItem("language"));
+  let lastLangEls = document.getElementsByClassName('lang-' + localStorage.getItem("language"));
   for (var i=0; i<lastLangEls.length; i++) {
-    var langEl = lastLangEls[i];
+    let langEl = lastLangEls[i];
     langEl.classList.remove('visible');
   }
   alert("Поточна мова сторінки: "+ $("input:radio:checked").val().toUpperCase());
-  var newlangEls = document.getElementsByClassName('lang-' + $("input:radio:checked").val());
+  let newlangEls = document.getElementsByClassName('lang-' + $("input:radio:checked").val());
   for (var i=0; i<newlangEls.length; i++) {
     var langEl = newlangEls[i];
     langEl.classList.add('visible');
   }
   localStorage.setItem("language",$("input:radio:checked").val());
+});
+
+$('#languages input').on('change', function() {
+  let lastLangEls = document.getElementsByClassName('lang');
+  for (var i=0; i<lastLangEls.length; i++) {
+    let langEl = lastLangEls[i];
+    langEl.classList.remove('visible');
+  }
+  let newlangEls = document.getElementsByClassName('lang-' + $("input:radio:checked").val());
+  for (var i=0; i<newlangEls.length; i++) {
+    var langEl = newlangEls[i];
+    langEl.classList.add('visible');
+  }
 });
