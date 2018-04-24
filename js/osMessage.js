f1764afa563ccd317ab52d9ee971885de8583088
Oleksandr1296 <olkravchukv@gmail.com>
2018-04-24 17:29:51 +0300

    fix task 3

--- js/osMessage.js
+++ js/osMessage.js
@@ -1,8 +1,23 @@
 const $version=$('#version');
+
+function osMassege () {
+  var inf = navigator.platform;
+
+  if (inf.search("Win") != -1) {
+    return "Windows";
+  }
+
+  else if (inf.search("Mac") != -1){
+    return "MacOS";
+  }
+
+  else if (inf.search("Lin") != -1){
+    return "LinuxOS";
+  }
+}
 $version.on('click',function () {
-  var platform = navigator.platform;
   let setPlatform = document.getElementById('platform');
-  setPlatform.innerText = platform;
+  setPlatform.innerText = osMassege ();
   $('.download-message').show();
   $version.hide();
 });
